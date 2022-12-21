88136378a7090beb73176e31c0391bb4d677ef07
Nivia <niviafalves@hotmail.com>
2022-12-21 18:16:19 -0300

    Criado modal contato

--- static/java_script.js
+++ static/java_script.js
@@ -1,47 +1,62 @@
 
-$(function(){
-    $("#modal_sobre").click(function(){
-        $("#modal-sob").modal();
-    });
-    });
-
-
-$(function() {
-    $("#modal_redes").click(function() {
-        $("#modal-cont"). modal();
-    });
-});
-
-    /* Função efeito escrita */
-    
-    /*
-    let i = 0;
-    let tag = document.getElementById("text");
-    let html = document.getElementById("text").innerHTML;
-    let attr = tag.setAttribute("data", html);
-    let txt = tag.getAttribute("data");
-    let speed = 170;
-    
-    function typeWriter() {
-        if (i <= txt.length) {
-            document.getElementById("text").innerHTML = txt.slice(0, i + 1);
-            i++
-            setTimeout(typeWriter, speed); 
-        }
-    }
-    typeWriter(); */
-
 const openSobreButton = document.querySelector("#open-sobre");
 const closeSobreButton = document.querySelector("#close-sobre");
 const modal = document.querySelector("#modal");
 const fade = document.querySelector("#fade");
+
 
 const toggleModal = () => {
   modal.classList.toggle("hide");
   fade.classList.toggle("hide");
 };
 
+
 [openSobreButton, closeSobreButton, fade].forEach((el) => {
   el.addEventListener("click", () => toggleModal());
 });
+
+const openContatoButton = document.querySelector("#open-contato");
+const closeContatoButton = document.querySelector("#close-contato");
+const modals = document.querySelector("#modals");
+const fades = document.querySelector("#fades");
+
+
+const toggleModals = () => {
+  modals.classList.toggle("hides");
+  fades.classList.toggle("hides");
+};
+
+
+
+[openContatoButton, closeContatoButton, fades].forEach((el) => {
+  el.addEventListener("click", () => toggleModals());
+});
+  
+ /* 
+// afetar outro elemento com mouseover
+var social = document.querySelector(".social");
+
+social.addEventListener("mouseover", function() {
+
+  var legenda = document.querySelector("#legenda");
+  var legenda1 = document.querySelector("#legenda1")
+  var legenda2 = document.querySelector("#legenda2")
+
+  legenda.classList.remove("hiden");
+  legenda1.classList.remove("hiden")
+  legenda2.classList.remove("hiden")
+
+});
+
+social.addEventListener("mouseout", function() {
+
+  var legenda = document.querySelector("#legenda");
+  var legenda1 = document.querySelector("#legenda1");
+  var legenda2 = document.querySelector("#legenda2");
+
+  legenda.classList.add("hiden");
+  legenda1.classList.add("hiden");
+  legenda2.classList.add("hiden");
+
+});*/
     
